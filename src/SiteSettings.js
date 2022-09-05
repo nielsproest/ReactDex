900990b119bf49f707ff78dee698e8872ecf6797
Mug <>
2022-09-05 12:37:57 +0200

    Added settings TODO

---
+++ src/SiteSettings.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//TOOD: Move global settings like theme here
+//And wrap most routes in this SettingsHolder
+//Move DConfig,SiteTheme from Base here
+class SettingsHolder extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (<div></div>)
+	}
+}
